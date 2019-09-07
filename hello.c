7e55d20377a414951b8de79ac35d18d6cebcb184
jackyztli <jacky.szu.edu.cn@gmail.com>
2019-09-07 06:03:19 +0000

    add file test

---
+++ hello.c
@@ -0,0 +1,11 @@
+// File Name: hello.c
+// Author: jacky
+// Created Time: Wed 04 Sep 2019 05:58:17 PM UTC
+
+#include "hello.h"
+#include <stdio.h>
+
+void sayHello()
+{
+    printf("This is jackyztli test project!\n");
+}
---
+++ hello.h
@@ -0,0 +1,4 @@
+#ifndef __HELLO__
+#define __HELLO__
+void sayHello();
+#endif
